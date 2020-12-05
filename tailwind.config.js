56371d7124a3a9762a14db82ad728d8ea11a56f9
Aamir Abbas <aamyear@gmail.com>
2020-12-06 00:31:49 +0400

    Blog Template
    - Blog template skeleton
    - @apply directives for nav menu

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3399CC",
+        secondary: "#EFA536",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
